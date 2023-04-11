e3537e35a9003e470a6389d2ff8a3bd75e9888d1
EpicBlue1 <21100115@virtualwindow.co.za>
2023-04-11 15:43:42 +0200

    Last photos added

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import image from "../../assets/sliderImages/image1.JPG";
+import image2 from "../../assets/sliderImages/image2.JPG";
+import image3 from "../../assets/sliderImages/image3.JPG";
+import image4 from "../../assets/sliderImages/image4.JPG";
+import image5 from "../../assets/sliderImages/image5.JPG";
+import image6 from "../../assets/sliderImages/image6.JPG";
+import image7 from "../../assets/sliderImages/image7.JPG";
 import ButtSlider from "./BtnSlider";
 import Style from "./landing.module.scss";
-import image from '../../assets/sliderImages/image1.JPG';
-import image2 from '../../assets/sliderImages/image2.JPG';
-import image3 from '../../assets/sliderImages/image3.JPG'
-import image4 from '../../assets/sliderImages/image4.JPG'
-import image5 from '../../assets/sliderImages/image5.JPG'
-import image6 from '../../assets/sliderImages/image6.JPG'
-import image7 from '../../assets/sliderImages/image7.JPG'
 
 const Landing = () => {
   const [active, setActive] = useState(true);
-  const [img, setimg] = useState(0)
-  const images = [image, image2, image3, image4, image5, image7]
+  const [img, setimg] = useState(0);
+  const images = [image, image2, image3, image4, image5, image7];
   function ActiveSwitch() {
     setActive(false);
   }
@@ -24,20 +24,22 @@ const Landing = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      img == images.length - 1 ? setimg(0) : setimg(img + 1)
-    }, 4500)
+      img == images.length - 1 ? setimg(0) : setimg(img + 1);
+    }, 4500);
 
-    console.log(image)
-  }, [img])
+    console.log(image);
+  }, [img]);
 
   return (
     <div className={Style.Container}>
-      <div className={Style.HeadSection}  style={{ backgroundImage: `url(${images[img]})`}}>
+      <div
+        className={Style.HeadSection}
+        style={{ backgroundImage: `url(${images[img]})` }}
+      >
         <div className={Style.SliderSec}>
           <div className={Style.SliderSubSec}>
             <h3 className={Style.welcome}>Welcome to,</h3>
-            <div className={Style.logoImg}>
-            </div>
+            <div className={Style.logoImg}></div>
             <br></br>
             <p className={Style.SliderSubSecPar}>
               Biotech is a perfect balance of high technology and physical
@@ -90,34 +92,36 @@ const Landing = () => {
               }
             >
               <div className={Style.ImageOne}></div>
-
-              <h2 className={Style.Margin}>WIRELESS CHARGING ARMBAND</h2>
-              <p>
-                Whether you’re on a hike or a bike, the wireless charging
-                armband ensures that your phone stays charged during any long
-                adventure or commute. This makes it the perfect addition to any
-                outdoor athlete’s life. DIRECTIONS:{" "}
-              </p>
-              <p>
-                1. Put on the armband by sliding your arm through the opening.
-                The charging pad should be on the inside of the armband and
-                should be in contact with your skin.{" "}
-              </p>
-              <p>
-                2. Place your smartphone or other compatible device onto the
-                charging pad. The charging pad should be positioned so that it
-                aligns with the device's charging port.{" "}
-              </p>
-              <p>
-                3. Wait for the device to start charging. You should see a
-                charging indicator light up on both the armband and the device.
-                The time it takes to charge will depend on the device's battery
-                capacity and the charging speed of the armband.{" "}
-              </p>
-              <p>
-                4. Once your device is fully charged, remove it from the armband
-                by sliding it off the charging pad.
-              </p>
+              <div className={Style.InstructionsContainer}>
+                <h2 className={Style.Margin}>WIRELESS CHARGING ARMBAND</h2>
+                <p>
+                  Whether you’re on a hike or a bike, the wireless charging
+                  armband ensures that your phone stays charged during any long
+                  adventure or commute. This makes it the perfect addition to
+                  any outdoor athlete’s life. DIRECTIONS:{" "}
+                </p>
+                <p>
+                  1. Put on the armband by sliding your arm through the opening.
+                  The charging pad should be on the inside of the armband and
+                  should be in contact with your skin.{" "}
+                </p>
+                <p>
+                  2. Place your smartphone or other compatible device onto the
+                  charging pad. The charging pad should be positioned so that it
+                  aligns with the device's charging port.{" "}
+                </p>
+                <p>
+                  3. Wait for the device to start charging. You should see a
+                  charging indicator light up on both the armband and the
+                  device. The time it takes to charge will depend on the
+                  device's battery capacity and the charging speed of the
+                  armband.{" "}
+                </p>
+                <p>
+                  4. Once your device is fully charged, remove it from the
+                  armband by sliding it off the charging pad.
+                </p>
+              </div>
             </div>
 
             <div
@@ -127,29 +131,31 @@ const Landing = () => {
                   : Style.InfoContainerHelmet
               }
             >
-              <div className={Style.ImageOne}></div>
-              <h2 className={Style.Margin}>BATTERY CHARGING HELMET</h2>
-              <p>
-                Whether you’re on a hike or a bike, the battery charging helmet
-                ensures that your bike lights and other devices remain charged
-                during your commute, making it a must-have accessory for any
-                outdoor athlete. Designed to harness the power of the sun, the
-                helmet is equipped with solar panels that convert sunlight into
-                energy to charge your devices. DIRECTIONS:
-              </p>
-              <p>
-                1. The battery get’s inserted into the holder on top of the
-                helmet.
-              </p>
-              <p>
-                2. Then, through the solar panel the sun charges the battery.
-              </p>
-              <p>
-                3. As soon as the battery is fully charged the user will be
-                notified and they can remove the battery and insert it into the
-                wireless charger, or any other item of their choice.
-              </p>
-              <p>4. The helmet also features a display that shows</p>
+              <div className={Style.ImageTwo}></div>
+              <div className={Style.InstructionsContainer}>
+                <h2 className={Style.Margin}>BATTERY CHARGING HELMET</h2>
+                <p>
+                  Whether you’re on a hike or a bike, the battery charging
+                  helmet ensures that your bike lights and other devices remain
+                  charged during your commute, making it a must-have accessory
+                  for any outdoor athlete. Designed to harness the power of the
+                  sun, the helmet is equipped with solar panels that convert
+                  sunlight into energy to charge your devices. DIRECTIONS:
+                </p>
+                <p>
+                  1. The battery get’s inserted into the holder on top of the
+                  helmet.
+                </p>
+                <p>
+                  2. Then, through the solar panel the sun charges the battery.
+                </p>
+                <p>
+                  3. As soon as the battery is fully charged the user will be
+                  notified and they can remove the battery and insert it into
+                  the wireless charger, or any other item of their choice.
+                </p>
+                <p>4. The helmet also features a display that shows</p>
+              </div>
             </div>
           </div>
         </h2>
